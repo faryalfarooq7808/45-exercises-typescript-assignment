f84631b2fbb6adc5c22169df61a3511af4da2cd8
faryalfarooq7808 <164650470+faryalfarooq7808@users.noreply.github.com>
2024-03-30 09:56:33 -0700

    exercises 1 to 10
    typescript assignment

---
+++ app.ts
@@ -0,0 +1,5 @@
+// Exercise 10
+// My name is Faryal Farooq
+//dated: 29/03/24
+//this program will print hello world program
+console.log('hello world program');
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Exercise 10
+// My name is Faryal Farooq
+//dated: 29/03/24
+//this program will do multiplication
+console.log(3 * 5);
